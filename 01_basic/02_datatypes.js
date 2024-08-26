7fc7f33f85087e3e0cee7c318982b47f08af01bd
Nabeel Khan <nabeelkhan2004@hotmail.com>
2024-08-26 22:04:04 +0000

    Update 02_datatypes.js and add source links in Readme

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -4,6 +4,24 @@
 
 console.log(3
     +
-    3) //code readability
+    3) //code readability should be high
 
 console.log("Nabeel Khan")
+
+let name = "Nabeel Khan";
+let age = 33;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); // null show typeof object in console
+console.log(typeof undefined); // Undefined type is undefined
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javascriptchaiorcode
 Javascript Practice
+
+Source https://tc39.es/ecma262/ & https://developer.mozilla.org/en-US/docs/Web/JavaScript

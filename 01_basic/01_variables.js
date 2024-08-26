96f14c148ceb3877403a333291a16d1082e4c9d2
Nabeel Khan <nabeelkhan2004@hotmail.com>
2024-08-26 21:25:06 +0000

    Add 01_variables.js file

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144540;
+let accountEmail = "nabeel.kk1990@gmail.com";
+var accountPassword = "12345";
+accountCity = "Karachi";
+
+//accountId = 2 // not allowed because constant variable cannot be reassign
+
+accountEmail = "nabeelkhan2004@hotmail.com";
+accountpassword = "21212121";
+accountCity = "Islamabad";
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])

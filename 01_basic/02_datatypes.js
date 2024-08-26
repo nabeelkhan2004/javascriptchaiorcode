038b000da084abb2de7deebee08f7587360a501e
Nabeel Khan <nabeelkhan2004@hotmail.com>
2024-08-26 21:41:21 +0000

    create 02_datatypes.js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability
+
+console.log("Nabeel Khan")

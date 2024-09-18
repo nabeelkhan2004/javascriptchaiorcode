2bf87b58861bb6183d032448fc507f89ac905ca3
Nabeel Khan <nabeelkhan2004@hotmail.com>
2024-09-18 19:12:24 +0000

    Learn While Loop & Do While Loop

---
+++ 05_Iterations/two.js
@@ -0,0 +1,35 @@
+//While Loop
+
+let i = 0;
+
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`)
+//     i = i + 2;
+// }
+
+// let myArray = ['flash', 'Batman', 'Superman']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr++
+// }
+
+//Do While Loop
+
+let score = 11
+
+// do {
+//     console.log(`Your score is ${score}`)
+//     score++
+// } while (score <= 10);
+
+// do {
+//     if (String(score).length === 1) {
+//         score = '0' + score
+//         Number(score)
+//         console.log(score);
+//     } else {
+//         console.log(score);
+//     }
+//     score++
+// } while (score <= 10);
